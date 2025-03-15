0bd135d6bb48eecd38404ad49194bcf28c63ca48
Pratyay360 <pratyaymustafi@gmail.com>
2025-03-15 18:15:48 +0530

    type script with shadcn **

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,18 @@
-import { useState } from 'react'
-import Navbar from './Components/Navbar'
-import Home from './Pages/Home.jsx'
-import { ThemeProvider } from "./Components/theme-provider"
-import Footer from './Components/Footer.js'
-import './App.css'
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./app/Navbar";
+import Home from "./Pages/Home";
+import Footer from "./app/Footer.js";
+import { ThemeProvider } from "./components/theme-provider"
 
 export default function App() {
-
   return (
-    <>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <Navbar></Navbar>
-        <Home></Home>
-        <Footer></Footer>
-      </ThemeProvider>
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <BrowserRouter>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    </BrowserRouter>
+    </ThemeProvider>
   );
 }
--- frontend/src/app/AboutUs.tsx
+++ frontend/src/app/AboutUs.tsx
@@ -1,7 +1,7 @@
 export default function AboutUs() {
   return (
     <>
-    
+    hii
     </>
   );
 }
